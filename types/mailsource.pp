34b28fba44734677ccd329634848ab00ae3b040d
Nomen Nescio <info@nomennesc.io>
2022-02-01 23:06:59 +0100

    retry

--- types/mailsource.pp
+++ types/mailsource.pp
@@ -1,17 +1,17 @@
 # @summary Manage a SOGo mail source
 type Sogo::Mailsource = Struct[
   {
-    SOGoDraftsFolderName             => Optional[String],
-    SOGoSentFolderName               => Optional[String],
-    SOGoTrashFolderName              => Optional[String],
-    SOGoIMAPServer                   => Optional[String],
-    SOGoSieveServer                  => Optional[String],
-    SOGoSMTPServer                   => Optional[String],
-    SOGoMailDomain                   => Optional[String],
-    SOGoMailingMechanism             => Optional[String],
-    SOGoForceExternalLoginWithEmail  => Optional[String],
-    SOGoMailSpoolPath                => Optional[String],
-    NGImap4ConnectionStringSeparator => Optional[String],
+    Optional[SOGoDraftsFolderName]             => String,
+    Optional[SOGoSentFolderName]               => String,
+    Optional[SOGoTrashFolderName]              => String,
+    Optional[SOGoIMAPServer]                   => String,
+    Optional[SOGoSieveServer]                  => String,
+    Optional[SOGoSMTPServer]                   => String,
+    Optional[SOGoMailDomain]                   => String,
+    Optional[SOGoMailingMechanism]             => String,
+    Optional[SOGoForceExternalLoginWithEmail]  => String,
+    Optional[SOGoMailSpoolPath]                => String,
+    Optional[NGImap4ConnectionStringSeparator] => String,
   }
 ]
 
